a9b94030704f4e5ae4866a8f67af9ccd71678cb3
sionelt <sltafengatoto@gmail.com>
2018-02-21 23:17:48 -0700

    Refactored Header in App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Header from './Header'
+import './App.css'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        hello world
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<Header />
+			</div>
+		)
+	}
 }
 
-export default App;
+export default App
+
