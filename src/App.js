0930c764b56d9e17b3621f82431a96eae9a9e354
sionelt <sltafengatoto@gmail.com>
2018-02-22 01:14:44 -0700

    Bind slide transitions to view my works button

--- src/App.js
+++ src/App.js
@@ -3,14 +3,45 @@ import Header from './Header'
 import './App.css'
 
 class App extends Component {
+	constructor() {
+		super()
+
+		this.state = {
+			isViewWorks: false,
+			viewWorksText: 'See My Works'
+		}
+
+		this.handleViewWorks = this.handleViewWorks.bind(this)
+	}
+
+	handleViewWorks(e) {
+		this.setState({
+			isViewWorks: !this.state.isViewWorks,
+			viewWorksText: this.state.isViewWorks ? 'See My Works' : 'Hide My Works'
+		})
+
+		this.fadeElements.forEach(each => (each.style.opacity = 0))
+
+		setTimeout(() => {
+			this.fadeElements.forEach(each => (each.style.opacity = 1))
+		}, 500)
+	}
+
+	componentDidMount() {
+		this.fadeElements = document.querySelectorAll('.fade')
+	}
+
 	render() {
 		return (
 			<div>
-				<Header />
+				<Header
+					onClick={this.handleViewWorks}
+					isViewWorks={this.state.isViewWorks}
+					viewWorksText={this.state.viewWorksText}
+				/>
 			</div>
 		)
 	}
 }
 
 export default App
-
