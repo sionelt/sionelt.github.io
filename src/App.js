1ea53cca2dcb682558612f12822f9e32a9e39c55
sionelt <sltafengatoto@gmail.com>
2018-03-02 01:09:33 -0700

    Added UI spinner while data is pending from firebase DB

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import Projects from './Projects'
 import './App.css'
 import * as firebase from 'firebase'
 
+const Pending = () => (
+	<div className="pending_container">
+		<i className="fas fa-circle-notch fa-spin fa-5x" />
+	</div>
+)
+
 class App extends Component {
 	constructor() {
 		super()
@@ -11,6 +17,7 @@ class App extends Component {
 		this.state = {
 			isViewWorks: false,
 			viewWorksText: 'See My Works',
+			dataIsPending: false,
 			homeData: {},
 			projectsData: []
 		}
@@ -22,11 +29,15 @@ class App extends Component {
 		this.setState({
 			isViewWorks: !this.state.isViewWorks,
 			viewWorksText: this.state.isViewWorks ? 'See My Works' : 'Hide My Works'
-		})
+      })
+      
+      // initial opacities
 		this.projectsContainer.style.opacity = 0
 		this.iconsNavMobile.style.opacity = 0
 		this.returnArrowMobile.style.opacity = 0
-		this.fadeElements.forEach(each => (each.style.opacity = 0))
+      this.fadeElements.forEach(each => (each.style.opacity = 0))
+      
+      //final opacities
 		setTimeout(() => {
 			this.fadeElements.forEach(each => (each.style.opacity = 1))
 			this.projectsContainer.style.opacity = 1
@@ -40,7 +51,12 @@ class App extends Component {
 		this.fadeElements = document.querySelectorAll('.fade')
 		this.projectsContainer = document.querySelector('.projects_container')
 		this.iconsNavMobile = document.querySelector('.icons_nav')
-		this.returnArrowMobile = document.querySelector('.return_arrow')
+      this.returnArrowMobile = document.querySelector('.return_arrow')
+      
+      // pending spinner when there's no data
+      this.setState({
+         dataIsPending: true
+      })
 
 		// data reads from firebase DB
 		firebase
@@ -48,36 +64,28 @@ class App extends Component {
 			.ref('/')
 			.once('value')
 			.then(snap => {
-				this.setState({
+            this.setState({
+               dataIsPending: false,
 					homeData: snap.val().home,
 					projectsData: snap.val().projects
 				})
-				console.log(snap.val())
 			})
 	}
 
 	render() {
 		return (
 			<div className="app_container">
+				{this.state.dataIsPending ? <Pending /> : ''}
 				<Header
 					header={this.state.homeData}
 					onClick={this.handleViewWorks}
 					isViewWorks={this.state.isViewWorks}
 					viewWorksText={this.state.viewWorksText}
 				/>
-            <Projects
-               projects={this.state.projectsData}
-               isViewWorks={this.state.isViewWorks}
-            />
+				<Projects projects={this.state.projectsData} isViewWorks={this.state.isViewWorks} />
 			</div>
 		)
 	}
 }
 
 export default App
-
-// TODO:
-// --- zoom out active or selected project card.
-// --- hook up with firebase DB and storage.
-// --- add projects to it.
-// --- deploy with github pages.
